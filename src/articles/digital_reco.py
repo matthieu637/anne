03fc4d35e0299d34377c478c9b6cf7cb2b2b45c7
Matthieu Zimmer <matthieu.zimmer@inria.fr>
2012-02-27 11:12:46 +0100

    a try to improve digital_reco.py curves

--- src/articles/digital_reco.py
+++ src/articles/digital_reco.py
@@ -16,7 +16,7 @@ from data import DataFile
 
 if __name__ == '__main__':
     mode = MultilayerNetwork.R0to1
-    nbr_network = 10
+    nbr_network = 3
     momentum = 0.5
     nbEpoch = 201
     display_interval = range(nbEpoch)[6::5]
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     networks = [{} for _ in range(nbr_network)]
     
     for i in range(nbr_network):
-        first_order = MultilayerNetwork(7, 100, 10, learning_rate=0.15, momentum=momentum, grid=mode)
+        first_order = MultilayerNetwork(7, 100, 10, learning_rate=0.15, momentum=momentum, grid=mode, gradient=0.9)
         high_order_h = MultilayerNetwork(100, 100, 2, learning_rate=0.1, momentum=momentum, grid=mode)
         high_order_l = MultilayerNetwork(100, 100, 2, learning_rate=10e-7, momentum=momentum, grid=mode)
         
@@ -35,9 +35,9 @@ if __name__ == '__main__':
 
     for network in networks:
         for k in network.keys():
+            if(k == 'first_order'):
                 network[k].init_random_weights(-.6, .6)
 
-    plt.title("Error of first-order and higher-order networks")
     #create example
     examples = DataFile("../data/digital_shape.txt", mode)
 
@@ -46,12 +46,22 @@ if __name__ == '__main__':
               'high_order_h' : [],
               'high_order_l': []}
 
+    y_perfo = {'first_order' : [] ,
+              'high_order_h' : [],
+              'high_order_l': []}
     #learning
     for epoch in range(nbEpoch):
         sum_rms = {'first_order' : 0. ,
                    'high_order_h' : 0.,
                    'high_order_l': 0.}
+        perfo = {'first_order' : [] ,
+                 'high_order_h' : [],
+                 'high_order_l': []}
         for network in networks:
+            perfo_i = {'first_order' : 0. ,
+                 'high_order_h' : 0.,
+                 'high_order_l': 0.}
+            
             l_exx = list(range(10))
             shuffle(l_exx)
             for ex in l_exx:
@@ -69,7 +79,12 @@ if __name__ == '__main__':
                 sum_rms['high_order_l'] += network['high_order_l'].calc_RMS(
                                             network['first_order'].stateHiddenNeurons,
                                             cell)
-                
+                if(findMax(network['first_order'].stateOutputNeurons) == findMax(examples.outputs[ex])):
+                    perfo_i['first_order'] += 1
+                if(findMax(network['high_order_h'].stateOutputNeurons) == findMax(cell)):
+                    perfo_i['high_order_h'] += 1
+                if(findMax(network['high_order_l'].stateOutputNeurons) == findMax(cell)):
+                    perfo_i['high_order_l'] += 1
                 
                 #learn
                 network['high_order_h'].train(network['first_order'].stateHiddenNeurons,
@@ -78,7 +93,14 @@ if __name__ == '__main__':
                                                cell)
                 network['first_order'].train(examples.inputs[ex],
                                              examples.outputs[ex])
-
+            
+            perfo['first_order'].append(perfo_i['first_order']/10)
+            perfo['high_order_l'].append(perfo_i['high_order_l']/10)
+            perfo['high_order_h'].append(perfo_i['high_order_h']/10)
+            
+        y_perfo['first_order'].append(sum(perfo['first_order']) / nbr_network )
+        y_perfo['high_order_h'].append(sum(perfo['high_order_h']) / nbr_network)
+        y_perfo['high_order_l'].append(sum(perfo['high_order_l']) / nbr_network)
 
         y_plot['first_order'].append(sum_rms['first_order'])
         y_plot['high_order_h'].append(sum_rms['high_order_h'])
@@ -95,15 +117,26 @@ if __name__ == '__main__':
         y_plot['high_order_l'][i] /= max_err[2]
     
     #displays
+    plt.title("Error of first-order and higher-order networks")
     plt.plot(display_interval , y_plot['first_order'][6::5], label="first-order network")
     plt.plot(display_interval , y_plot['high_order_h'][6::5], label="high-order network (high learning rate)")
     plt.plot(display_interval , y_plot['high_order_l'][6::5], label="high-order network (low learning rate)")
     plt.ylabel('ERROR')
     plt.xlabel("EPOCHS")
     plt.axis((0, nbEpoch, 0, 1.))
-    plt.legend()
+    plt.legend(loc='best', frameon=False)
     plt.show()
     
+
+    plt.title("Performance of first-order and higher-order networks")
+    plt.plot(display_interval , y_perfo['first_order'][6::5], label="first-order network")
+    plt.plot(display_interval , y_perfo['high_order_h'][6::5], label="high-order network (high learning rate)")
+    plt.plot(display_interval , y_perfo['high_order_l'][6::5], label="high-order network (low learning rate)")
+    plt.ylabel('ERROR')
+    plt.xlabel("EPOCHS")
+    plt.axis((0, nbEpoch, 0, 1.))
+    plt.legend(loc='best', frameon=False)
+    plt.show()
     
     #testing
     for ex in range(10):
