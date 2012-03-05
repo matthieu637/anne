5bd02d6ca67fd37c97d373f7a0278b45a272ea64
Matthieu Zimmer <matthieu.zimmer@inria.fr>
2012-03-05 18:31:09 +0100

    still bugged

--- src/articles/blindslight.py
+++ src/articles/blindslight.py
@@ -8,23 +8,28 @@ Created on 24 fevr. 2012
 from data import DataFile
 from random import shuffle
 from network import MultilayerNetwork
-from neuron import Neuron, NeuronR0to1
+from neuron import Neuron, NeuronN0to1, NeuronR0to1
 from utils import index_max
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+   
     r = {'x':(0, 1),
          '.': (0,),
          '?': (0, 0.02),
          '!': (1,)}
     samples = DataFile("../data/blindslight.txt", rules=r)
+#    samples.inputs = samples.inputs[0:100]
     
-    first_order = MultilayerNetwork(100, 60, 100, 0, 0.9, 0, 1., False, True)
-    first_order.init_random_weights(-1, 1)
     
-    high_order = [Neuron(100, 0.1, 0., 1., Neuron.Output, False, True) for _ in range(2)]
-    high_order[0].init_random_weights(0, 0.1)
-    high_order[1].init_random_weights(0, 0.1)
+    first_order = MultilayerNetwork(100, 60, 100, 0, 0.9, 0.4, 2., False, True)
+    first_order.init_random_weights(-0.6, 0.6)
+    
+    
+#    high_order = [NeuronR0to1(100, 0.1, 0., 1., Neuron.Output, False, True) for _ in range(2)]
+    high_order = [NeuronN0to1(100, 0.1, 0., False) for _ in range(2)]
+    high_order[0].init_random_weights(0., 0.1)
+    high_order[1].init_random_weights(0., 0.1)
 
 #    print(samples.inputs[101])
 #    print(samples.outputs[101])
@@ -32,69 +37,90 @@ if __name__ == '__main__':
     err = 0.
     the = 0.
     rms = []
-    all_cmp = []
+    rms2 = []
     
+    nbEpoch = 150
     #pre-training
-    for epoch in range(30):
-        l_exx = list(range(200))
+    for epoch in range(nbEpoch):
+        l_exx = list(range(len(samples.inputs)))
         shuffle(l_exx)
         rms_ss = 0.
+        rms_ss2 = 0.
         for ex in l_exx:
 #        for ex in l_exx[1:150]:
-            first_order.train(samples.inputs[ex], samples.outputs[ex])
-#            first_order.calc_output(samples.inputs[ex])
+
+#            rms_ss2 += first_order.calc_RMS(samples.inputs[ex], samples.outputs[ex])
+#            first_order.train(samples.inputs[ex], samples.outputs[ex])
+            first_order.calc_output(samples.inputs[ex])
         
-            compara = []
+            #compara
+            compara=[]
             for i in range(100):
-                compara.append((samples.inputs[ex][i] - first_order.stateOutputNeurons[i] ))
-#            all_cmp.append(compara)
+                compara.append((samples.inputs[ex][i] - first_order.stateOutputNeurons[i]))
+
+            res2 = [high_order[i].calc_output(list(compara))
+                    for i in range(2)]
+            if(index_max(res2) == 0):
+                err += 1
+            
             
             i = index_max(first_order.stateOutputNeurons)
-            j = index_max(samples.inputs[ex])
-            if ((first_order.stateOutputNeurons[i] > 0.5 and samples.inputs[ex][j] > 0.5 and i == j) \
-                or(first_order.stateOutputNeurons[i] <= 0.5 and samples.inputs[ex][j] <= 0.5)) :
+            j = index_max(samples.outputs[ex])
+            if ((first_order.stateOutputNeurons[i] > 0.5 and samples.outputs[ex][j] > 0.5 and i==j ) \
+                or(first_order.stateOutputNeurons[i] <= 0.5 and samples.outputs[ex][j] <= 0.5)) :
+
 #            i = index_max(samples.outputs[ex])
 #            j = index_max(samples.inputs[ex])
 #            if ((samples.outputs[ex][i] > 0.5 and samples.inputs[ex][j] > 0.5 and i == j) \
 #                    or(samples.outputs[ex][i] <= 0.5 and samples.inputs[ex][j] <= 0.5)) :
-                high_order[0].train(compara, 1)
-                high_order[1].train(compara, -1)
-                
-                res = [high_order[i].calc_output(compara)
-                    for i in range(2)]
-            
-                if(index_max(res) == 1):
-                    rms_ss +=1
-            else:
-                the += 1
-                high_order[0].train(compara, -1)
-                high_order[1].train(compara, 1)
-                
+
                 res = [high_order[i].calc_output(compara)
                     for i in range(2)]
             
                 if(index_max(res) == 0):
                     rms_ss +=1
+                    
+                high_order[0].train(compara, 1.)
+                high_order[1].train(compara, 0.)
+            else:
+#                print(compara)
+                the += 1
+                
+                res = [high_order[i].calc_output(compara)
+                    for i in range(2)]
+#                print(res)
+            
+                if(index_max(res) == 1):
+                    rms_ss +=1
+                    
+                high_order[0].train(compara, 0.)
+                high_order[1].train(compara, 1.)
 #                print()
         
 #            if(min(compara) >= 1.):
 #                print(min(compara))
 #                exit()
+            first_order.train(samples.inputs[ex], samples.outputs[ex])
+            
         rms.append(rms_ss/200)
+        rms2.append((200-the)/200)
 #            if((index_max(first_order.calc_output(samples.inputs[ex]))!=index_max(samples.inputs[ex]) and  ( max(samples.inputs[ex]) > .5 or max(first_order.calc_output(samples.inputs[ex])) > .5 )) \
 #                  or (max(first_order.calc_output(samples.inputs[ex])) <= 0.5 and max(samples.inputs[ex]) > 0.5 ) \
 #                  or (max(first_order.calc_output(samples.inputs[ex])) > 0.5 and max(samples.inputs[ex]) < 0.5 )):
 #                err += 1
                 
-#            first_order.train(samples.inputs[ex], samples.outputs[ex])
-        print(epoch, err, the)
+            
+        print(epoch, "perf 1st : ", (200 - the), " | perf 2nd : ",  rms_ss, " | high wag %d" % err)
         err = 0.
         the = 0.
 
 #    print(all_cmp)
 
     plt.plot(rms,
+             label="high-order network")
+    plt.plot(rms2,
              label="first-order network")
+    plt.axis((0, nbEpoch, 0, 1.))
     plt.legend(loc='best', frameon=False)
     plt.show()
 
@@ -107,46 +133,37 @@ if __name__ == '__main__':
              'lw_in' : 0,
              'co': 0,
              'in': 0}
-    err = 0
     
     for ex in range(200):
         first_order.calc_output(samples.inputs[ex])
         
-        if((index_max(first_order.calc_output(samples.inputs[ex]))==index_max(samples.outputs[ex]) and max(samples.outputs[ex]) > 0.5) \
-            or(max(first_order.stateOutputNeurons) <= 0.5 and max(samples.inputs[ex]) <= 0.5)):
-            pourc['co'] += 1
-        else :
-            pourc['in'] += 1
-        
-        compara = []
+        #compara
+        compara=[]
         for i in range(100):
-            compara.append(samples.inputs[ex][i] - first_order.stateOutputNeurons[i])
-            
-        res = [high_order[i].calc_output(compara)
-                    for i in range(2)]
+            compara.append((samples.inputs[ex][i] - first_order.stateOutputNeurons[i]))
+
+        res33 = [high_order[i].calc_output(compara) for i in range(2)]
+        
+#        print(first_order.stateOutputNeurons)
+#        print(res33, high_order[0].bias, sum(compara))
             
         i = index_max(first_order.stateOutputNeurons)
-        j = index_max(samples.inputs[ex])
-        if ((first_order.stateOutputNeurons[i] > 0.5 and samples.inputs[ex][j] > 0.5 and i == j) \
-                or(first_order.stateOutputNeurons[i] <= 0.5 and samples.inputs[ex][j] <= 0.5)) :
-#        
-#        i = index_max(samples.outputs[ex])
-#        j = index_max(samples.inputs[ex])
-#        if ((samples.outputs[ex][i] > 0.5 and samples.inputs[ex][j] > 0.5 and i == j) \
-#                or(samples.outputs[ex][i] <= 0.5 and samples.inputs[ex][j] <= 0.5)) :
-            err += 1
-            if index_max(res) == 0:#high wager
+        j = index_max(samples.outputs[ex])
+        if ((first_order.stateOutputNeurons[i] > 0.5 and samples.outputs[ex][j] > 0.5 and i == j) \
+                or(first_order.stateOutputNeurons[i] <= 0.5 and samples.outputs[ex][j] <= 0.5)) :
+            pourc['co'] += 1
+            if index_max(res33) == 0:#high wager
                 pourc['hg_co'] += 1
             else:
                 pourc['lw_co'] += 1
         else:
-            
-            if index_max(res) == 0:#high wager
+            pourc['in'] += 1
+            if index_max(res33) == 0:#high wager
                 pourc['hg_in'] += 1
             else:
                 pourc['lw_in'] += 1
             
-    print(pourc, err, pourc['lw_co'], pourc['lw_in'])
+    print(pourc, pourc['lw_co'], pourc['lw_in'])
     
 
 #    #Subthreshold stimuli
