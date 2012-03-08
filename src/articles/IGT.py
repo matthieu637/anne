f495a60059813540a1d1ae9ae51ffa2f9aad35e3
Matthieu Zimmer <matthieu.zimmer@inria.fr>
2012-03-08 15:50:41 +0100

    IGT finished

--- src/articles/IGT.py
+++ src/articles/IGT.py
@@ -14,17 +14,17 @@ import matplotlib.pyplot as plt
 
 def deck(nbr):
     if(nbr in (0, 2)):
-        return random() < 0.7
+        return (True, random() < 0.7)
     else: 
-        return random() < 0.3
+        return (False, random() < 0.3)
 
 if __name__ == '__main__':
     
+    plt.title("low")
     grid = MultilayerNetwork.R0to1
-    nbr_network = 5
+    nbr_network = 30
     nbr_epoch = 200
     nbr_trial = 10
-    display_interval = range(nbr_epoch)[::5]
     
     first_order = []
     high_order = []
@@ -49,28 +49,33 @@ if __name__ == '__main__':
         nbr_gwager = 0
         for net in range(nbr_network):
             for trial in range(nbr_trial):
-        #        print(last_output)
-#                for i in range(len(last_output[net])):
-#                    last_output[net][i] += randmm(0, 0.02)
+                for i in range(len(last_output[net])):
+                    last_output[net][i] += randmm(0.01, 0.03)
         
                 bet = index_max(first_order[net].calc_output(last_output[net]))
+                
+                for i in range(len(first_order[net].stateHiddenNeurons)):
+                        first_order[net].stateHiddenNeurons[i] += randmm(0.01, 0.03)
+                
                 wager = index_max(high_order[net].calc_output(first_order[net].stateHiddenNeurons))
                 
                 win = deck(bet)
                 
-                if(win):
+                if((win[0] and wager == 0) ):
+                        nbr_gwager += 1
+                
+                if(win[0]):                    
+                    high_order[net].train(first_order[net].stateHiddenNeurons, [1. , grid])
+                    
                     outputs = [grid for _ in range(4)] + [1.]
                     outputs[bet] = 1.
                     first_order[net].train(outputs, outputs)
                     last_output[net] = outputs
                     
                     nbr_win += 1
-                    
-                    if(wager == 0):
-                        nbr_gwager += 1
-                    
-                    high_order[net].train(first_order[net].stateHiddenNeurons, [1. , grid])
                 else:
+                    high_order[net].train(first_order[net].stateHiddenNeurons, [grid , 1.])
+                    
                     inputs = [grid for _ in range(4)] + [grid]
                     inputs[bet] = 1.
                     outputs = [1. for _ in range(4)]
@@ -78,18 +83,14 @@ if __name__ == '__main__':
                     first_order[net].train(inputs, outputs)
                     last_output[net] = inputs
                     
-                    if(wager == 1):
-                        nbr_gwager += 1
-                    
-                    high_order[net].train(first_order[net].stateHiddenNeurons, [grid , 1.])
         
-        
+        print(epoch)
         wins.append(nbr_win / (nbr_network * nbr_trial))
         wagers.append(nbr_gwager / (nbr_network * nbr_trial))
 
-    plt.plot(display_interval, [wins[i] for i in display_interval],
+    plt.plot(wins,
              label="first-order network")
-    plt.plot(display_interval, [wagers[i] for i in display_interval],
+    plt.plot(wagers,
              label="high-order network")
     plt.axis((0, nbr_epoch, 0, 1.))
     plt.legend(loc='best', frameon=False)
