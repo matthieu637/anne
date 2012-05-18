2abe609f010dc204d8eb8094ccc1f64d35fb4394
Matthieu Zimmer <zimme0511@etumail.uhp-nancy.fr>
2012-05-18 12:29:23 +0200

    simulation of wagering with a reinforcement learning

--- src/articles/wagering/handwritten_preinforcement.py
+++ src/articles/wagering/handwritten_preinforcement.py
@@ -10,8 +10,8 @@ from multilayerp import MultilayerPerceptron
 from utils import index_max
 from random import shuffle, seed
 import matplotlib.pyplot as plt
+from prenforcement import PRenforcement
 from data import DataFile
-
 
 if __name__ == '__main__':
     mode = MultilayerPerceptron.R0to1
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         seed(i)
         first_order = MultilayerPerceptron(16 * 16, 100, 10, learning_rate=0.15, momentum=momentum, grid=mode)
         first_order.init_weights_randomly(-1, 1)
-        high_order_h = MultilayerPerceptron(100, 20, 2, learning_rate=0.1, momentum=0., grid=mode)
+        high_order_h = PRenforcement(100, temperature=1.)
         
         networks[i] = {'first_order' : first_order,
                     'high_order_h' : high_order_h}
@@ -56,24 +56,23 @@ if __name__ == '__main__':
             shuffle(l_exx)
             for ex in l_exx[0:nbTry]:
                 network['first_order'].calc_output(examples.inputs[ex])
-                cell = [mode, 1] \
+                cell = 1 \
                         if index_max(network['first_order'].stateOutputNeurons) == index_max(examples.outputs[ex]) \
-                        else [1, mode]
+                        else 0
                 
-                network['high_order_h'].calc_output(network['first_order'].stateHiddenNeurons)
+                res = network['high_order_h'].calc_output(network['first_order'].stateHiddenNeurons)
 
                 if(index_max(network['first_order'].stateOutputNeurons) == index_max(examples.outputs[ex])):
                     perfo['first_order'] += 1
-                if(index_max(network['high_order_h'].stateOutputNeurons) == index_max(cell)):
+                if(res == cell):
                     perfo['high_order_h'] += 1
                     
                     
-                if(index_max(network['high_order_h'].stateOutputNeurons) == 1):
+                if(res == 1):
                     perfo['high_order_l'] += 1
                 
                 #learn
-                network['high_order_h'].train(network['first_order'].stateHiddenNeurons,
-                                               cell)
+                network['high_order_h'].train(res == cell)
                 network['first_order'].train(examples.inputs[ex],
                                              examples.outputs[ex])
             
@@ -84,7 +83,7 @@ if __name__ == '__main__':
         print(epoch)
     
 
-    plt.title("Performance of first-order and higher-order networks")
+    plt.title("Performance of first-order and higher-order networks (prenforcement)")
     plt.plot(display_interval , y_perfo['first_order'][3::5], label="first-order network", linewidth=2)
     plt.plot(display_interval , y_perfo['high_order_h'][3::5], label="high-order network (high learning rate)", linewidth=2)
     plt.plot(display_interval , y_perfo['high_order_l'][3::5], label="proportion of high wagers")
